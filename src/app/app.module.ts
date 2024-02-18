7d564225d70abadd59a35d078c32c623277939bd
Yago Moreira <yagomoreira@gmail.com>
2024-02-18 09:23:49 -0300

    add menu-title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuTitleComponent } from './menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuTitleComponent,
   ],
   imports: [
     BrowserModule,
