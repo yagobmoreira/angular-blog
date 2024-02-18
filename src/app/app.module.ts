85b5d0cddde8189d73980019269279d5ec2f6e25
Yago Moreira <yagomoreira@gmail.com>
2024-02-18 11:20:39 -0300

    add menu-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
 import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { SmallCardComponent } from './components/small-card/small-card.component
     HomeComponent,
     BigCardComponent,
     SmallCardComponent,
+    MenuBarComponent,
   ],
   imports: [
     BrowserModule,
